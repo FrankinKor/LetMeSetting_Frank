bbd04a625ed41fc976f5dcc9a8c0bcadcf37efbe
FrankinKor <tgifrank32@gmail.com>
2023-08-12 21:10:37 +0900

    viwpager layout + circle indicator(grey) 로 만듬

---
+++ app/src/main/java/com/example/letmesetting_frank/viewpager_1.kt
@@ -0,0 +1,11 @@
+package com.example.letmesetting_frank
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class viewpager_1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_viepager1)
+    }
+}
